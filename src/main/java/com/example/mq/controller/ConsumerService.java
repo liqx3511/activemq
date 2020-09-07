6d03623dbe7ba2da8458d32d136b63726b667aae
liqingxiao <15008139511@163.com>
2020-09-07 10:45:23 +0800

    activemq: 消费者名字修改

--- src/main/java/com/example/mq/controller/ConsumerService.java
+++ src/main/java/com/example/mq/controller/ConsumerService.java
@@ -7,7 +7,7 @@ import javax.jms.Session;
 import javax.jms.TextMessage;
 
 @Component
-public class ConsumerController {
+public class ConsumerService {
 
     // 使用JmsListener配置消费者监听的队列，其中name是接收到的消息
     @JmsListener(destination = "testQueue1", containerFactory = "jmsQueueListener")
